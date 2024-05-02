22cd8592a3fb53193488c41d9999f3003f0722a1
July58 <perevozniukjv@gmail.com>
2024-05-02 17:25:33 +0300

    create animal repository

---
+++ src/main/java/com/example/test_task/repository/AnimalRepository.java
@@ -0,0 +1,9 @@
+package com.example.test_task.repository;
+
+import com.example.test_task.model.Animal;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AnimalRepository extends JpaRepository<Animal, Long> {
+}
